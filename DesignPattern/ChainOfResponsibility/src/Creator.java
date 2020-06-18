bfe2150e655ec921e49abc6f10a75ddf1970cba3
Andrea Fornaia <fornaia@dmi.unict.it>
2020-06-18 13:40:18 +0200

    feat: Add chain example

---
+++ DesignPattern/ChainOfResponsibility/src/Creator.java
@@ -0,0 +1,18 @@
+
+public class Creator {
+
+	private Creator() {}
+
+	public static HelpDesk basicHelpDesk() {
+		return new HDLivello1();
+	}
+	
+	public static HelpDesk businessHelpDesk() {
+		return new HDLivello1(new HDLivello2());
+	}
+	
+	public static HelpDesk premiumHelpDesk() {
+		return new HDLivello1(new HDLivello2(new HDLivello3()));
+	}
+
+}
---
+++ DesignPattern/ChainOfResponsibility/src/HelpDesk.java
@@ -0,0 +1,19 @@
+
+public abstract class HelpDesk {
+	
+	private HelpDesk successor = null;
+	
+	public HelpDesk(HelpDesk successor) {
+		this.successor = successor;
+	}
+
+	public void sendTicket(String ticket) {
+		if (successor != null) { // se sono qui, la richiesta non è stata gestita, la passo al successore
+			successor.sendTicket(ticket); 
+		} else { // siamo arrivati alla fine della catena, nessuno ha gestito la richiesta
+			System.out.println("Ticket: " + ticket);
+			System.out.println("- Il suo piano tariffario non copre il servizio di assistenza richiesto. Buona giornata.");
+		}
+	}
+
+}
